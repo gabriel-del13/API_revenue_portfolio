9d0859fe38bbf3e627c68601477ad99575cd8999
Gabriel <gaby13453@gmail.com>
2025-11-02 18:55:56 -0500

    feat: :white_check_mark: fix users models password

--- users/models.py
+++ users/models.py
@@ -4,7 +4,6 @@ class Client(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.TextField(null=False)
     email = models.EmailField(unique=True, null=False)
-    password = models.TextField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
